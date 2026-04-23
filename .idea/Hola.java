c88c82a193e9ff6f5cf4619de1efb94b57991cc4
alfonsovillegasmoreno6-hash <alfonsovillegasmoreno6@gmail.com>
2026-04-23 17:23:33 +0200

    Add new print statement to Hola.java

--- .idea/Hola.java
+++ .idea/Hola.java
@@ -1,6 +1,7 @@
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola mundo modificado");
+         System.out.println("Hola mundo 2");
     }
     System.out.println("Nueva línea");
 }
